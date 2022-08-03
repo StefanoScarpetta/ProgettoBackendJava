cee72764d120fcece4b349710e017b63557f45b1
Stefano Scarpetta <scarpettastefano.main@gmail.com>
2022-08-03 10:13:47 +0200

    Download file json in local
    Working download class to download json file from a link in a local file

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import url.Download;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-            System.out.println("Ciao");
+
+    public static void main(String[] args) throws Exception {
+
+        String link = "https://webgate.ec.europa.eu/comp/redisstat/api/dissemination/sdmx/2.1/data/comp_ag_x$comp_ag_01?format=json&compressed=false";
+        File out = new File("C:\\Users\\stefa\\Documents\\GitHub\\ProgettoBackendJava\\url\\Scaricato.json");
+        new Thread(new Download(link, out)).start(); //da approfondire Thread() e .start();
+
     }
+
 }
---
+++ url/Download.java
@@ -0,0 +1,35 @@
+package url;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Download implements  Runnable{ //Approfondire Runnable
+    String link;
+    File out;
+    public Download(String link, File out) {
+        this.link = link;
+        this.out = out;
+    }
+
+    @Override
+    public void run() {
+        try{
+            URL url = new URL(link);
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            BufferedInputStream in = new BufferedInputStream(http.getInputStream());
+            FileOutputStream fos = new FileOutputStream(this.out);
+            BufferedOutputStream bout = new BufferedOutputStream(fos,1024);
+            byte[] buffer = new byte[1024];
+            int read = 0;
+            while((read = in.read(buffer, 0, 1024)) >= 0) {
+                bout.write(buffer, 0, read);
+            }
+            bout.close();
+            in.close();
+            System.out.println("Downloaded complete");
+        }catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
